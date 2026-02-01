ba50a951a0f3dc3f21e00389acca18a29404ee31
bukenalen30 <zenicbelpha91@gmail.com>
2026-02-01 19:06:06 +0700

    Update app or model

--- app.py
+++ app.py
@@ -51,8 +51,7 @@ with col2:
         <div style="
             text-align:center; 
             background: linear-gradient(90deg, #ffeb3b, #ff5722, #2196f3, #4caf50, #e91e63);
-            margin-top:20px
-            padding:40px 20px; 
+            padding:20px; 
             border-radius:12px; 
             border:2px solid #ffc107;
             margin-bottom:20px ">
