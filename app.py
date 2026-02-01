5018e8dc9bfd9c30cafbe207c33ca8c04a253ee7
bukenalen30 <zenicbelpha91@gmail.com>
2026-02-01 19:01:30 +0700

    Update app or model

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ with col2:
         <div style="
             text-align:center; 
             background: linear-gradient(90deg, #ffeb3b, #ff5722, #2196f3, #4caf50, #e91e63);
-            margin-top: 50px
-            padding:10px 20px 25px 20px; 
+            margin-top: 20px
+            padding:10px ; 
             border-radius:12px; 
             border:2px solid #ffc107;
-            margin-bottom:50px ">
+            margin-bottom:20px ">
             <h1 style="margin:5px 0 0 0; line-height:1.2; color:white;">Roblox Popularity Classifier 📊</h1>
             <p style="margin:5px 0 0 0; font-size:16px; line-height:1.2; color:white;">Prediksi tingkat popularitas game Roblox menggunakan model SVM & KNN</p>
         </div>
