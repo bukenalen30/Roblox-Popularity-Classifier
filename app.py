548f8d96423efa9998618c06dfc547f0aa967c62
bukenalen30 <zenicbelpha91@gmail.com>
2026-02-03 20:23:03 +0700

    update streamlit app

--- app.py
+++ app.py
@@ -290,10 +290,10 @@ if svm_matrix is not None or knn_matrix is not None:
     colA, colB = st.columns(2)
     with colA:
         if svm_matrix is not None:
-            plot_matrix(svm_matrix, "Confusion Matrix - SVM", cmap_color="YlGn")
+            plot_matrix(svm_matrix, "Confusion Matrix - SVM", cmap_color="Green")
     with colB:
         if knn_matrix is not None:
-            plot_matrix(knn_matrix, "Confusion Matrix - KNN", cmap_color="YlGnBu")
+            plot_matrix(knn_matrix, "Confusion Matrix - KNN", cmap_color="Blue")
 
 # ==============================================
 # TAMPILKAN CLASSIFICATION REPORT LENGKAP + Accuracy & Weighted F1
