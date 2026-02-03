c2fe3d6bf8d32e85839def2410b795e5a7a487e7
bukenalen30 <zenicbelpha91@gmail.com>
2026-02-03 15:35:14 +0700

    Update app or model

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ st.markdown(
 col1, col2, col3 = st.columns([1, 4, 1])
 with col1:
     st.write("")
- with col2:
+with col2:
     st.markdown("<br><br><br>", unsafe_allow_html=True)
     st.markdown(
         """
