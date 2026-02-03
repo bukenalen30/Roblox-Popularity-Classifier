461cb6a2a88b998b1686d73b3c889effd3f70314
bukenalen30 <zenicbelpha91@gmail.com>
2026-02-03 16:13:37 +0700

    Update app or model

--- app.py
+++ app.py
@@ -59,37 +59,39 @@ st.markdown(
 # CERIA HEADER
 # ==============================================
 
-col1, col2, col3 = st.columns([1, 6, 1])
+st.markdown(
+    """
+    <div style="
+        display:flex;
+        align-items:center;
+        justify-content:space-between;
+        gap:20px;
+        margin-top:40px;
+        margin-bottom:20px;
+    ">
+        <img src="logo_left.png" style="width:120px; height:auto;">
 
-with col1:
-    st.image("ysalen.png", width=80)
-
-with col2:
-    st.markdown("<br><br>", unsafe_allow_html=True)
-    st.markdown(
-        """
         <div style="
-            text-align:center; 
+            flex:1;
+            text-align:center;
             background: linear-gradient(90deg, #ff5722, #ff7043, #ffb74d, #ffd54f, #fff176);
-            padding:20px; 
-            border-radius:20px; 
+            padding:25px;
+            border-radius:20px;
             border:8px solid #0d47a1;
-            margin-bottom:20px;
         ">
-            <h1 style="margin:5px 0; line-height:1.2; color:white; font-size:38px;">
+            <h1 style="margin:5px 0; line-height:1.2; color:white; font-size:40px;">
                 Roblox Popularity Classifier 📊
             </h1>
             <p style="margin:5px 0; font-size:18px; line-height:1.2; color:white;">
                 Prediksi tingkat popularitas game Roblox menggunakan model SVM & KNN
             </p>
         </div>
-        """,
-        unsafe_allow_html=True
-    )
 
-with col3:
-    st.image("salen.png", width=80)
-
+        <img src="salen.png" style="width:120px; height:auto;">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # ==============================================
 # LOAD MODEL DAN RESOURCE
